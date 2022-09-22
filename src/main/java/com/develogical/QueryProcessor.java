63095f6d911baabde5c4a7030c9b9902b5b3284d
Kevin Wu <kevinwu2@andrew.cmu.edu>
2022-09-22 15:30:53 -0400

    stuff

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,11 +3,13 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+        System.out.println("Processing: " + query);
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        }if (query.toLowerCase().contains("what is your name")) {
+        }
+        if (query.toLowerCase().contains("what is your name")) {
             return "Waffle";
         }
         return "";
